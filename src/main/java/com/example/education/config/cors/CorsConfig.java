581293960f58a6d02cc7dcb8f0b993aa6cd7e95f
turkanerehimzade <turkanerehimzade09@gmail.com>
2025-07-08 13:24:28 +0400

    add

--- src/main/java/com/example/education/config/cors/CorsConfig.java
+++ src/main/java/com/example/education/config/cors/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
