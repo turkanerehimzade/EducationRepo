0925258a7d0cbba01e6df3894a5d82d04b3dda8f
turkanerehimzade <turkanerehimzade09@gmail.com>
2025-08-17 18:30:10 +0400

    added

--- src/main/java/com/example/education/controller/CourseController.java
+++ src/main/java/com/example/education/controller/CourseController.java
@@ -1,11 +1,14 @@
 package com.example.education.controller;
 
 import com.example.education.dto.request.course.CourseRequest;
+import com.example.education.dto.response.PageResponse;
 import com.example.education.dto.response.base.SuccessResponse;
 import com.example.education.dto.response.course.CourseResponse;
 import com.example.education.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +18,12 @@ public class CourseController {
     @GetMapping("/{id}")
     public SuccessResponse<CourseResponse> getCourseById(@PathVariable Long id) {
         return courseService.getCourseById(id);
+    }
+    @GetMapping
+    public SuccessResponse<PageResponse<CourseResponse>> getAllCourses(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size) {
+        return courseService.getAllCourses(page,size);
     }
 
 }
--- src/main/java/com/example/education/service/CourseService.java
+++ src/main/java/com/example/education/service/CourseService.java
@@ -6,6 +6,7 @@ import com.example.education.dao.repository.CourseRepository;
 import com.example.education.dao.repository.MaterialRepository;
 import com.example.education.dao.repository.QuestionRepository;
 import com.example.education.dto.request.course.CourseRequest;
+import com.example.education.dto.response.PageResponse;
 import com.example.education.dto.response.base.SuccessResponse;
 import com.example.education.dto.response.course.CourseResponse;
 import com.example.education.dto.response.material.MaterialResponse;
@@ -13,6 +14,10 @@ import com.example.education.enums.ResponseCode;
 import com.example.education.mapper.CourseMapper;
 import com.example.education.mapper.MaterialMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,6 +28,7 @@ import java.util.stream.Collectors;
 public class CourseService {
     private final CourseRepository courseRepository;
     private final MaterialRepository materialRepository;
+    private final CourseMapper courseMapper;
     private final QuestionRepository questionRepository;
 
     public SuccessResponse<CourseResponse> getCourseById(Long id){
@@ -41,4 +47,28 @@ public class CourseService {
         courseRepository.save(course);
         return SuccessResponse.createSuccessResponse(CourseMapper.INSTANCE.entityToResponse(course), ResponseCode.SUCCESS);
     }
+    public SuccessResponse<PageResponse<CourseResponse>> getAllCourses(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        Page<Course> courses = courseRepository.findAll(pageable);
+
+        List<CourseResponse> courseResponses = courses
+                .getContent()
+                .stream()
+                .map(courseMapper::entityToResponse)
+                .collect(Collectors.toList());
+
+        return SuccessResponse.createSuccessResponse(
+                new PageResponse<>(
+                        courseResponses,
+                        courses.getNumber(),
+                        courses.getSize(),
+                        courses.getTotalElements(),
+                        courses.getTotalPages(),
+                        courses.isFirst(),
+                        courses.isLast()
+                ),
+                ResponseCode.SUCCESS
+        );
+    }
 }
+
